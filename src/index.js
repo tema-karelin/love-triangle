9f55fc473f21657da42722248c3d28e2ff517b56
Artem Karelin <temi41995@gmail.com>
2019-03-22 02:45:28 +0300

    feat: love triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
+  let n = 1;
+  let main = 0;
+  while (n <= preferences.length) {
+    let number_2 = preferences[n-1];
+    let number_3 = preferences[number_2 - 1];
+    if (n === preferences[number_3 - 1]) {
+      main++;
+      preferences[n - 1] = NaN;
+      preferences[number_2 - 1] = NaN;
+      preferences[number_3 - 1] = NaN;
+    }
+    n++;
+  } 
+  return main;
   // your implementation
 };
